1aefc6bdf68d79478e72bb30d780b8be2e745a61
bdzynda2 <david.zynda14@gmail.com>
2017-10-28 14:52:29 -0600

    adding hw3 - partially finished

---
+++ Unfinished/HW3_AmericanOptionsPricer.py
@@ -0,0 +1,68 @@
+#HW 3 - The first options pricer for American Options
+import numpy as np
+from scipy.stats import binom
+
+class VanillaOption(object):
+    """An abstract interface for plain vanilla options"""
+
+    def __init__(self, strike, expiry):
+        self.strike = strike
+        self.expiry = expiry
+
+    def payoff(self, spot):
+        pass
+
+class VanillaCallOption(VanillaOption):
+    """A concrete class for vanilla call options"""
+
+    def payoff(self, spot):
+        return np.maximum(spot - self.strike, 0.0)
+
+    
+class VanillaPutOption(VanillaOption):
+    """A concrete class for vanilla put options"""
+
+    def payoff(self, spot):
+        return np.maximum(self.strike - spot, 0.0)
+
+
+
+def EuropeanBinomialPricer(option, spot, rate, vol, div, steps):
+    h = option.expiry / steps
+    nodes = steps + 1
+    u = np.exp((rate - div) * h + vol * np.sqrt(h))
+    d = np.exp((rate - div) * h - vol * np.sqrt(h))
+
+    p = (np.exp((rate - div) * h) - d) / (u - d)
+    disc = np.exp(-(rate - div))
+    callT = 0.0
+
+    for i in range(nodes):
+        spotT = spot * (u ** (steps - i)) * (d ** i)
+        callT += option.payoff(spotT) * binom.pmf(steps - i, steps, p)
+
+    price = callT * disc
+    return price
+ 
+
+
+def AmericanBinomialPricer(option, spot, rate, vol, div, steps):
+    h = option.expiry / steps
+    nodes = steps + 1
+    u = np.exp((rate - div) * h + vol * np.sqrt(h))
+    d = np.exp((rate - div) * h - vol * np.sqrt(h))
+
+    p = (np.exp((rate - div) * h) - d) / (u - d)
+    disc = np.exp(-(rate - div))
+    St = np.zeros((nodes, nodes))
+
+    for i in range(nodes):
+        St[i,steps] = spot * (u ** (steps - i)) * (d ** i)
+        #callT += option.payoff(spotT) * binom.pmf(steps - i, steps, p)
+
+    for i in range(steps - 1, -1, -1):
+        for j in range(i+1):
+            St[j,i] = St[j,i+1] / u
+
+    print(St)
+            
