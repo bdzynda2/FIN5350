6c08feac7b4ad64a106bff6e4ceec2085a841058
David Zynda <david.zynda14@gmail.com>
2017-10-28 16:51:58 -0600

    Moonage Daydream

--- HW3_OptionPricing/HW3_AmericanOptionsPricer.py
+++ HW3_OptionPricing/HW3_AmericanOptionsPricer.py
@@ -53,16 +53,24 @@ def AmericanBinomialPricer(option, spot, rate, vol, div, steps):
     d = np.exp((rate - div) * h - vol * np.sqrt(h))
 
     p = (np.exp((rate - div) * h) - d) / (u - d)
-    disc = np.exp(-(rate - div))
     St = np.zeros((nodes, nodes))
+    callPutT = np.zeros((nodes, nodes))
 
     for i in range(nodes):
         St[i,steps] = spot * (u ** (steps - i)) * (d ** i)
-        #callT += option.payoff(spotT) * binom.pmf(steps - i, steps, p)
+        
+    callPutT[:,[steps]] = option.payoff(St[:,[steps]]) 
 
     for i in range(steps - 1, -1, -1):
         for j in range(i+1):
             St[j,i] = St[j,i+1] / u
-
-    print(St)
             
+    for i in range(steps - 1, -1, -1):
+        for j in range(i+1):
+            callPutT[j,i] = np.maximum(np.exp(-(rate * h)) * ((p * callPutT[j, i+1]) + ((1 - p) * callPutT[j+1, i+1])), 
+                    option.payoff(St[j,i]))
+            
+           
+    
+    print("\n The price of the American Option is: $ {0:.6f}".format(callPutT[0,0]))
+ 
